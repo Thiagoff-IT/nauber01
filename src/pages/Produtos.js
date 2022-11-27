e87eafdcf67a87d10d7e940ffe4aa8e0a8d9bdce
Thiago <thiagodsantos316@gmail.com>
2022-11-26 22:28:44 -0300

    Add files via upload

---
+++ src/pages/Produtos.js
@@ -0,0 +1,245 @@
+import "./Produtos.css";
+
+const Produtos = () => {
+  return (
+    <div className="produtos-div34">
+      <div className="footer-div22">
+        <div className="footer-div23">
+          <div className="bg-div12" />
+          <img className="phone-icon33" alt="" src="../phone33.svg" />
+          <div className="siga-nas-redes-sociais11">Siga nas redes sociais</div>
+          <div className="nauber-2022-todos-os-direito11">
+            Nauber © 2022 Todos os direitos reservados.
+          </div>
+          <div className="politicas-de-privacidade-ter11">
+            Politicas de Privacidade | Termos de Uso
+          </div>
+          <div className="nauber-maquinas-e-equipamentos11">
+            20.633.257/0001-98 | NAUBER MAQUINAS E EQUIPAMENTOS
+          </div>
+          <img
+            className="redes-sociais-icon11"
+            alt=""
+            src="../redes-sociais-icon7.svg"
+          />
+          <div className="dvidas-div11">
+            <div className="converse-com-um-especialista11">
+              Converse com um especialista
+            </div>
+            <div className="envie-um-e-mail11">Envie um e-mail</div>
+            <div className="voltar-ao-incio11">Voltar ao início</div>
+            <div className="ficou-com-dvidas11">Ficou com dúvidas?</div>
+          </div>
+          <div className="produtos-div35">
+            <div className="construo-civil-div11">Construção civil</div>
+            <div className="linha-industrial-div12">Linha industrial</div>
+            <div className="ficou-com-dvidas11">Produtos</div>
+          </div>
+          <div className="nossa-sede-div22">
+            <div className="rua-mal-castelo-branco-247712">
+              <p className="rua-mal-castelo11">
+                Rua Mal. Castelo Branco, 2477 Centro
+              </p>
+              <p className="schroeder-sc11">Schroeder - SC.</p>
+            </div>
+            <div className="div56">+55 (47) 3374-5969</div>
+            <div className="div57">+55 (11) 96364-6599</div>
+            <div className="div58">+55 (47) 98821-2725</div>
+            <div className="nossa-sede-div23">Nossa sede</div>
+          </div>
+          <img className="pin-icon12" alt="" src="../pin11.svg" />
+          <div className="line-div29" />
+          <img
+            className="logo-nauber-icon11"
+            alt=""
+            src="../logo-nauber@2x.png"
+          />
+          <div className="rectangle-div271" />
+        </div>
+      </div>
+      <div className="header-div11">
+        <div className="bg-header34" />
+        <div className="bg-header35" />
+        <div className="bg-header36" />
+        <div className="logo-div11">
+          <img
+            className="nauber-logo-1-icon11"
+            alt=""
+            src="../nauberlogo-1@2x.png"
+          />
+          <img
+            className="logo-nauber-111"
+            alt=""
+            src="../logo-nauber-1@2x.png"
+          />
+        </div>
+        <div className="menu-div11">
+          <img className="vector-icon64" alt="" src="../vector-83.svg" />
+          <div className="home-div12">
+            <b className="home-b11">HOME</b>
+            <img className="home-icon11" alt="" src="../home11.svg" />
+          </div>
+          <div className="empresa-div11">
+            <img className="empresa-icon11" alt="" src="../empresa.svg" />
+            <b className="empresa-b11">EMPRESA</b>
+          </div>
+          <div className="produtos-div37">
+            <b className="produtos-b11">PRODUTOS</b>
+            <img className="box-icon11" alt="" src="../box11.svg" />
+          </div>
+          <div className="assistencia-div11">
+            <b className="assistncias-b12">ASSISTÊNCIAS</b>
+            <img className="home-icon11" alt="" src="../assistencias2.svg" />
+          </div>
+          <div className="catlogos-div13">
+            <b className="catlogos-virtuais-b12">CATÁLOGOS VIRTUAIS</b>
+            <img className="home-icon11" alt="" src="../catalagos1.svg" />
+          </div>
+          <div className="contato-div12">
+            <b className="contato-b12">CONTATO</b>
+            <img className="home-icon11" alt="" src="../contato5.svg" />
+          </div>
+          <div className="login-div11">
+            <b className="login-b11">login</b>
+            <img className="logim-icon11" alt="" src="../logim.svg" />
+          </div>
+        </div>
+        <div className="flags-div11">
+          <img className="vector-icon65" alt="" />
+          <img
+            className="flag-brasil-1-icon11"
+            alt=""
+            src="../flagbrasil-11@2x.png"
+          />
+          <img
+            className="flag-espanha-1-icon11"
+            alt=""
+            src="../flagespanha-1@2x.png"
+          />
+          <img className="flag-eua-1-icon11" alt="" src="../flageua-1@2x.png" />
+          <img
+            className="flag-arabia-1-icon11"
+            alt=""
+            src="../flagarabia-11@2x.png"
+          />
+        </div>
+        <div className="line-div30" />
+        <img
+          className="base-de-conhecimento-111"
+          alt=""
+          src="../base-de-conhecimento-1@2x.png"
+        />
+        <img
+          className="logo-revenda-pro-111"
+          alt=""
+          src="../logo-revenda-pro-1@2x.png"
+        />
+        <div className="conhea-o-sistema-de-revnda-n11">
+          <p className="rua-mal-castelo11">
+            <span className="conhea-o-sistema23">
+              <span>CONHEÇA O SISTEMA</span>
+            </span>
+          </p>
+          <p className="rua-mal-castelo11">
+            <span className="conhea-o-sistema23">
+              <span>DE REVÊNDA</span>
+            </span>
+          </p>
+          <p className="nauber-pro13">
+            <span>
+              <b>NAUBER PRO</b>
+            </span>
+          </p>
+        </div>
+        <div className="seach-div11">
+          <div className="rectangle-div272" />
+          <img className="frame-icon10" alt="" src="../frame-24.svg" />
+        </div>
+      </div>
+      <div className="linha-1-div7">
+        <div className="produto-div6">
+          <div className="rectangle-div273" />
+          <div className="rectangle-div274" />
+          <img className="icon31" alt="" />
+          <b className="d-30x-b30">d-3.0x</b>
+        </div>
+        <div className="produto-div7">
+          <div className="rectangle-div275" />
+          <div className="rectangle-div274" />
+          <img className="icon31" alt="" />
+          <b className="d-30x-b30">d-3.0x</b>
+        </div>
+        <div className="produto-div8">
+          <div className="rectangle-div273" />
+          <div className="rectangle-div278" />
+          <img className="icon33" alt="" />
+          <b className="d-30x-b32">d-3.0x</b>
+          <img className="icon34" alt="" />
+        </div>
+      </div>
+      <div className="linha-2-div1">
+        <div className="produto-div6">
+          <div className="rectangle-div273" />
+          <div className="rectangle-div274" />
+          <img className="icon31" alt="" />
+          <b className="d-30x-b30">d-3.0x</b>
+        </div>
+        <div className="produto-div7">
+          <div className="rectangle-div275" />
+          <div className="rectangle-div274" />
+          <img className="icon31" alt="" />
+          <b className="d-30x-b30">d-3.0x</b>
+        </div>
+        <div className="produto-div8">
+          <div className="rectangle-div273" />
+          <div className="rectangle-div278" />
+          <img className="icon33" alt="" src="../01-16@2x.png" />
+          <b className="d-30x-b32">d-3.0x</b>
+          <img className="icon38" alt="" src="../1-1@2x.png" />
+        </div>
+      </div>
+      <div className="mquinas-div7">
+        <img className="vector-icon66" alt="" src="../vector-11710.svg" />
+        <img className="vector-icon67" alt="" src="../vector-11610.svg" />
+        <b className="mquinas-para-pintura13">máquinas para pintura</b>
+      </div>
+      <div className="menu-div12">
+        <img className="linha-icon" alt="" src="../linha.svg" />
+        <div className="tipo-de-produto">
+          <b className="categorias-b">tipo de produto</b>
+          <div className="airless-diafragma-7523">
+            <span>{`airless diafragma `}</span>
+            <span className="span">(7523)</span>
+          </div>
+          <div className="airless-pisto-7523">
+            <span>{`airless pistão `}</span>
+            <span className="span">(7523)</span>
+          </div>
+          <div className="rectangle-div285" />
+          <div className="rectangle-div286" />
+        </div>
+        <div className="categorias-div">
+          <b className="categorias-b">Categorias</b>
+          <div className="mquinas-para-pintura14">Máquinas para pintura</div>
+          <div className="lixadeiras-div">Lixadeiras</div>
+          <div className="aspiradores-div">Aspiradores</div>
+          <div className="misturadores-div">Misturadores</div>
+          <div className="fresadora-de-parede10">Fresadora de parede</div>
+          <div className="lixas-e-abrasivos6">Lixas e abrasivos</div>
+          <div className="peas-div">Peças</div>
+          <div className="acessrios-div">Acessórios</div>
+          <div className="rectangle-div287" />
+          <div className="rectangle-div288" />
+          <div className="rectangle-div289" />
+          <div className="rectangle-div290" />
+          <div className="rectangle-div291" />
+          <div className="rectangle-div292" />
+          <div className="rectangle-div293" />
+          <div className="rectangle-div294" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Produtos;
